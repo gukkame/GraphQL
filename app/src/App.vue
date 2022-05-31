947525a0f2a3462d56a139d7703f06afc8f65b38
gukka <guka245@gmail.com>
2022-05-31 22:04:42 +0300

    graphql

---
+++ app/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HomeView from "/src/views/HomeView.vue";
+</script>
+
+<template>
+  <HomeView />
+</template>
+
+<script>
+export default {
+  components: {
+    HomeView,
+  },
+};
+</script>
+
+<style>
+@import "@/assets/base.css";
+</style>
